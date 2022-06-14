eafc914c9ce3c6607318551dc88948ff28848938
Minura Punchihewa <minurapunchihewa17@gmail.com>
2022-06-14 22:35:01 +0530

    added mariadb connection

--- app/main/views.py
+++ app/main/views.py
@@ -3,8 +3,39 @@ from app.main import main
 from flask import request, jsonify, abort
 from flask import current_app
 import logging
+import mariadb
+import sys
 
 
 @main.route('/hello', methods=['GET'])
 def hello():
     return 'Hello World'
+
+
+@main.route('/databases', methods=['GET'])
+def databases():
+    print("databases route")
+    # Connect to MariaDB Platform
+    # try:
+    conn = mariadb.connect(
+        user="cc_user",
+        password="password",
+        host="mariadb",
+        port=3306
+
+    )
+    # except mariadb.Error as e:
+    #     print(f"Error connecting to MariaDB Platform: {e}")
+    #     sys.exit(1)
+
+    # Get Cursor
+    cur = conn.cursor()
+
+    cur.execute(
+        "SHOW DATABASES;",
+    )
+
+    for row in cur:
+        print(row)
+
+    return "databases"
--- resources/init/01.sql
+++ resources/init/01.sql
@@ -1,1 +1,7 @@
 CREATE DATABASE IF NOT EXISTS `community_cupboard`;
+USE community_cupboard;
+
+CREATE TABLE test(
+    col_1 INT,
+    col_2 INT
+);
